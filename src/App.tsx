d5fd7648628d2134fc2554ba5b177de4e41642ec
Bigali <104558475+Pandarry@users.noreply.github.com>
2023-06-11 20:55:08 +0600

    code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { Counter } from './components/Counter'
+import { Suspense } from 'react'
+import { Route, Routes, Link } from 'react-router-dom'
+import { AboutPageAsync } from './pages/AboutPage/AboutPageAsync'
+import { MainPageAsync } from './pages/MainPage/MainPageAsync'
 import './index.scss'
 
 export const App = () => {
     return (
         <div className="app">
-            <Counter />
+            <Link to="/">Главная страница</Link>
+            <Link to="/about">О сайте</Link>
+            <Suspense fallback={<div>...Loading</div>}>
+                <Routes>
+                    <Route path="/about" element={<AboutPageAsync />} />
+                    <Route path="/" element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     )
 }
