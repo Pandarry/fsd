7bc4110a73309b3d6b4a442a97bb1fac1570d5bd
Bigali <104558475+Pandarry@users.noreply.github.com>
2023-06-12 14:45:59 +0600

    add applink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,15 +6,16 @@ import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from 'app/providers/ThemeProvider'
 import { AboutPage } from 'pages/AboutPage'
 import { MainPage } from 'pages/MainPage'
-import { AppRouter } from './providers/router'
+import { AppRouter } from 'app/providers/router'
+import { Navbar } from 'widgets/Navbar'
 
 export const App = () => {
     const { theme, toggleTheme } = useTheme()
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar />
             <button onClick={toggleTheme}>ToggleTheme</button>
-            <Link to="/">Главная страница</Link>
-            <Link to="/about">О сайте</Link>
+
             <AppRouter />
         </div>
     )
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,20 @@
+import { classNames } from 'shared/lib/classNames/classNames'
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
+import styles from './Navbar.module.scss'
+
+interface NavbarProps {
+    className?: string
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+    return (
+        <div className={classNames(styles.navbar, {}, [className])}>
+            <div className={styles.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/">
+                    Главная страница
+                </AppLink>
+                <AppLink to="/about">О сайте</AppLink>
+            </div>
+        </div>
+    )
+}
