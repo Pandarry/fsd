6df148c988c1300d1ff523e45efa5d26acf7fab7
Bigali <104558475+Pandarry@users.noreply.github.com>
2023-06-13 15:51:32 +0600

    add page404 and pageloader

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,8 @@
+import { useTranslation } from 'react-i18next'
+import { classNames } from 'shared/lib/classNames/classNames'
+import styles from './NotFoundPage.module.scss'
+
+export function NotFoundPage() {
+    const { t } = useTranslation('')
+    return <div className={classNames(styles.NotFoundPage, {}, [])}>{t('Страница не найдена')}</div>
+}
